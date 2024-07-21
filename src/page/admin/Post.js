8e864bd3546271dfa80b00eb53c36f40cece4b49
2602hoang <vuhuyhoangboj@gmail.com>
2024-07-21 13:10:06 +0700

    fix

--- src/page/admin/Post.js
+++ src/page/admin/Post.js
@@ -6,8 +6,9 @@ function Post() {
   return (
     <LayoutAdmin>
         <div className='flex min-h-screen justify-center items-center overflow-hidden w-full flex-col'>
-          <h1 className='text-3xl font-bold text-center text-red-500 uppercase my-4'>Demo 70% Website</h1>
           <div className='flex justify-center items-center flex-col  min-h-screen'>
+          <h1 className='text-3xl font-bold text-center text-red-500 uppercase my-5'>Demo 70% Website</h1>
+
           <video width="750" height="500" controls >
           <source src="https://res.cloudinary.com/dzldyflpv/video/upload/v1721538469/AD6AC114-004E-4FC2-890D-3C387647C9DE_online-video-cutter.com_z4q3gm.mp4" type="video/mp4"/>
          </video>
