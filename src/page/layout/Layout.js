c7c8ec7e6aafed32d070f7b6dcced71a60a0b7b0
2602hoang <vuhuyhoangboj@gmail.com>
2024-08-12 22:41:40 +0700

    button

--- src/page/layout/Layout.js
+++ src/page/layout/Layout.js
@@ -5,6 +5,8 @@ import ProgressBar from '../../component/ProgressBar.js';
 import { AuthContext } from '../../contexts/AuthContextProvider.js';
 // import { Spin } from 'antd';
 import {SpinnerLayout} from '../../component/SpinnerLayout.js';
+import { FloatButton } from 'antd';
+import { CommentOutlined, FacebookOutlined, InstagramOutlined, MessageOutlined, PhoneFilled, XOutlined } from '@ant-design/icons';
 
 function Layout({ children }) {
   const { cart } = useContext(AuthContext);
@@ -26,6 +28,48 @@ function Layout({ children }) {
         <div className='overflow-hidden h-auto w-full flex flex-col justify-center items-center'>
           <ProgressBar />
           <Header cart={cart} />
+          <FloatButton.Group 
+            trigger="click"
+           
+            type='primary'
+            tooltip="Liên Hệ"
+            icon={<MessageOutlined />}
+            className="" shape="circle" style={{ left: 10, bottom: "30%" }}>
+              <FloatButton
+                tooltip="Facebook "
+                target="_blank"
+                
+                href=""
+                // style={{backgroundColor: "blue",color:"blue"}}
+                icon={<FacebookOutlined style={{color:"blue"}}/>}
+              />
+              <FloatButton
+                tooltip="X "
+                target="_blank"
+                // style={{backgroundColor: "black",color:"black"}}
+                href=""
+                icon={<XOutlined />}
+              />
+                <FloatButton
+                tooltip="Instagram "
+                target="_blank"
+                href=""
+                icon={<InstagramOutlined style={{color:"#8A3AB9"}}/>}
+              />
+              <FloatButton
+                tooltip="Nhắn Cho Tôi"
+                icon={<CommentOutlined style={{color:"#2596be"}}/>}
+                target="_blank"
+                href=""
+              />
+              <FloatButton
+               tooltip="Gọi Cho tôi"
+               icon={<PhoneFilled style={{color:"green"}}/>}
+               target="_blank"
+               href=""
+             />
+              {/* <FloatButton.BackTop visibilityHeight={0} /> */}
+            </FloatButton.Group>
           {children}
           <Footer />
         </div>
