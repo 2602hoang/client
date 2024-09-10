4315920bbad19e67461240d985a402abdc2f0941
hoang2602 <vuhuyhoangboj@gmail.com>
2024-09-10 19:53:11 +0700

    server

--- src/url/index.js
+++ src/url/index.js
@@ -1,3 +1,3 @@
-export const URL ='https://server-api-git-master-2602hoangs-projects.vercel.app/';
+export const URL = "https://server-api-mauve.vercel.app/";
 // https://server-api-git-master-2602hoangs-projects.vercel.app/
 // http://127.0.0.1:8080/
